d3d18945dec45dff1c1adf858e38652bc07be8a8
Maximo-Bertolin <maximobertolin@gmail.com>
2025-07-14 16:07:51 -0300

    Se termino el primer ejercicio

---
+++ ejercicios_excepcion/Ejercicio1/codigo_ejercicio/Division.java
@@ -0,0 +1,10 @@
+package codigo_ejercicio;
+
+public class Division {
+	public static double division(int dividendo, int divisor) throws ArithmeticException{
+		if(divisor == 0) {
+			throw new ArithmeticException();
+		}
+		return (double) dividendo / divisor;
+	}
+}
--- patrones_de_diseño_composite/src/testing/TestImprimirOrganigrama.java
+++ patrones_de_diseño_composite/src/testing/TestImprimirOrganigrama.java
@@ -1,6 +1,6 @@
 package testing;
 
-import static org.junit.jupiter.api.Assertions.*;
+//import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
